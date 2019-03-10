0c4b9c46fcc897950d7ff05ea58a9bbb8d5ecd64
neel1998 <neellovepainting@gmail.com>
2019-03-10 23:00:11 +0530

    sign up layout created

--- app/src/main/java/com/neel/agora/SignUpActivity.java
+++ app/src/main/java/com/neel/agora/SignUpActivity.java
@@ -6,17 +6,18 @@ import android.os.Bundle;
 import android.support.v7.widget.AppCompatTextView;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-        AppCompatTextView signUpText = findViewById(R.id.signup_text);
-        signUpText.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_sign_up);
+
+        AppCompatTextView loginText = findViewById(R.id.login_text);
+        loginText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
