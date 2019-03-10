7a4cfafecfc3f32bc758d8e3c26a1343e5b6e2b4
neel1998 <neellovepainting@gmail.com>
2019-03-11 03:39:26 +0530

    home activity layout created

--- app/src/main/java/com/neel/agora/HomeActivity.java
+++ app/src/main/java/com/neel/agora/HomeActivity.java
@@ -4,12 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class HomeActivity extends AppCompatActivity {
 
+    String token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Log.d("token", getIntent().getStringExtra("token"));
+        try {
+            JSONObject jsonObject = new JSONObject(getIntent().getStringExtra("token"));
+            token = jsonObject.getString("token");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        Log.d("token",token);
     }
 }
